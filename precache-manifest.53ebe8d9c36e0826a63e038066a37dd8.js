9518e509a5d2fd9d3d9bed74ed6a45fd7ea9fcb0
Anam Ahmed <anam.ahmed.a@gmail.com>
2020-07-04 01:18:39 +0600

    Updates

---
+++ precache-manifest.53ebe8d9c36e0826a63e038066a37dd8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff841e90ba559587024d2efdad0566e",
+    "url": "/react-awesome-lightbox/index.html"
+  },
+  {
+    "revision": "e65d1930a0b29c989735",
+    "url": "/react-awesome-lightbox/static/css/main.973b50ec.chunk.css"
+  },
+  {
+    "revision": "246a6e7f54134938a4f0",
+    "url": "/react-awesome-lightbox/static/js/2.5e92eece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-awesome-lightbox/static/js/2.5e92eece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65d1930a0b29c989735",
+    "url": "/react-awesome-lightbox/static/js/main.acd29c55.chunk.js"
+  },
+  {
+    "revision": "4081abf25753cad70a67",
+    "url": "/react-awesome-lightbox/static/js/runtime-main.a52ff741.js"
+  },
+  {
+    "revision": "2832f30d1d823a0a5bfefff67c4c7dec",
+    "url": "/react-awesome-lightbox/static/media/arrow.2832f30d.svg"
+  },
+  {
+    "revision": "a53683b121935bcd262e9a78378ed093",
+    "url": "/react-awesome-lightbox/static/media/close.a53683b1.svg"
+  },
+  {
+    "revision": "c0a385009286ec644156d12403085982",
+    "url": "/react-awesome-lightbox/static/media/reload.c0a38500.svg"
+  },
+  {
+    "revision": "6b8343333bd0d9e2785348aacfd0f4f4",
+    "url": "/react-awesome-lightbox/static/media/spinner.6b834333.svg"
+  },
+  {
+    "revision": "f088f0208a955f9c8fc273963e59e0bb",
+    "url": "/react-awesome-lightbox/static/media/ui-zoom-in.f088f020.svg"
+  },
+  {
+    "revision": "c2c46d23376f7868c8d836c5c4f5909b",
+    "url": "/react-awesome-lightbox/static/media/ui-zoom-out.c2c46d23.svg"
+  },
+  {
+    "revision": "2440673b2ddd2ee30e4f1994d995f497",
+    "url": "/react-awesome-lightbox/static/media/undo.2440673b.svg"
+  }
+]);
